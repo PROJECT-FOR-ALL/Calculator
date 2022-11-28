518ab716e020bf151b0dc31da4bfb13b14cd7afb
KantapatS <llbumpbumpll@hotmail.com>
2022-11-28 18:54:33 +0700

    อัพ Voice

--- calculatorapp/lib/home.dart
+++ calculatorapp/lib/home.dart
@@ -7,268 +7,247 @@ import 'package:math_expressions/math_expressions.dart';
 class First extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-	  return MaterialApp(
-	  debugShowCheckedModeBanner: false,
-	  //home: SET(),
-	  ); // MaterialApp
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home: SET(),
+    ); // MaterialApp
   }
 }
 
 class SET extends StatefulWidget {
   @override
-    FirstScreen createState() => FirstScreen();
+  FirstScreen createState() => FirstScreen();
 }
 
-class FirstScreen extends State<SET>{
+class FirstScreen extends State<SET> {
   @override
   var userInput = '';
   var answer = '';
 
 // Array of button
-final List<String> buttons = [
-	'AC',
-	'C',
-	'%',
-	'/',
-	'7',
-	'8',
-	'9',
-	'x',
-	'4',
-	'5',
-	'6',
-	'-',
-	'1',
-	'2',
-	'3',
-	'+',
-	' ',
-	'0',
-	'.',
-	'=',
-];
+  final List<String> buttons = [
+    'AC',
+    'C',
+    '%',
+    '/',
+    '7',
+    '8',
+    '9',
+    'x',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    ' ',
+    '0',
+    '.',
+    '=',
+  ];
 
-@override
-Widget build(BuildContext context) {
-
-	return Scaffold(
-	appBar: AppBar(
-    title: Text(
-      "CalculatorForAll",
-      style: TextStyle(
-        fontSize: 25,
-        color: Color(0xFF57636C)
-        )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CalculatorForAll",
+            style: TextStyle(fontSize: 25, color: Color(0xFF57636C))),
+        backgroundColor: Color(0xFFF1F4F8),
+        iconTheme: IconThemeData(color: Color(0xFF57636C)),
+        actions: [
+          PopupMenuButton(
+              //icon: Icon(Icons.more_vert,color: Color(0xFF57636C)),
+              itemBuilder: (context) {
+            return [
+              PopupMenuItem<int>(
+                value: 0,
+                child:
+                    Text("Speech", style: TextStyle(color: Color(0xFF57636C))),
+              ),
+              PopupMenuItem<int>(
+                value: 1,
+                child: Text("Calculator",
+                    style: TextStyle(color: Color(0xFF57636C))),
+              ),
+            ];
+          }, onSelected: (value) {
+            if (value == 0) {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => MyApp()));
+            } else if (value == 1) {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => SET()));
+            }
+          }),
+        ],
       ),
-
-    backgroundColor: Color(0xFFF1F4F8),
-    iconTheme: IconThemeData(color: Color(0xFF57636C)),
-    actions: [
-    PopupMenuButton(
-      //icon: Icon(Icons.more_vert,color: Color(0xFF57636C)),
-      itemBuilder: (context){
-        return [
-          PopupMenuItem<int>(
-              value: 0,
-              child: Text("Speech",
-              style: TextStyle(color: Color(0xFF57636C))),
+      backgroundColor: Color(0xFFF1F4F8),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        userInput,
+                        style:
+                            TextStyle(fontSize: 18, color: Color(0xFF57636C)),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        answer,
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Color(0xFF101213),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ]),
+            ),
           ),
-          PopupMenuItem<int>(
-              value: 1,
-              child: Text("Calculator",
-              style: TextStyle(color: Color(0xFF57636C))),
-          ),
-        ];
-      },
-      onSelected:(value){
-        if(value == 0){
-          Navigator.push(context,
-           MaterialPageRoute(builder: (context)=>MyApp()));
-        }else if(value == 1){
-          Navigator.push(context,
-           MaterialPageRoute(builder: (context)=>SET()));
-        }
-      }
-    ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    // AC = All Clear Button
+                    if (index == 0) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput = '';
+                            answer = '0';
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Color(0xFF7BCFFF),
+                        textColor: Color.fromARGB(255, 255, 255, 255),
+                      );
+                    }
 
-    ],
-  ),
+                    // C = Delete Button
+                    else if (index == 1) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput =
+                                userInput.substring(0, userInput.length - 1);
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Color(0xFF7BCFFF),
+                        textColor: Color.fromARGB(255, 255, 255, 255),
+                      );
+                    }
 
-	backgroundColor: Color(0xFFF1F4F8),
-	body: Column(
-		children: <Widget>[
-		Expanded(
-			child: Container(
-			child: Column(
-				mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-				children: <Widget>[
-					Container(
-					padding: EdgeInsets.all(20),
-					alignment: Alignment.centerRight,
-					child: Text(
-						userInput,
-						style: TextStyle(fontSize: 18, color: Color(0xFF57636C)),
-					),
-					),
-					Container(
-					padding: EdgeInsets.all(15),
-					alignment: Alignment.centerRight,
-					child: Text(
-						answer,
-						style: TextStyle(
-							fontSize: 30,
-							color: Color(0xFF101213),
-							fontWeight: FontWeight.bold),
-					),
-					)
-				]),
-			),
-		),
+                    // % Button
+                    else if (index == 2) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput += buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Color(0xFF7BCFFF),
+                        textColor: Color.fromARGB(255, 255, 255, 255),
+                      );
+                    }
 
-		Expanded(
-			flex: 3,
-			child: Container(
-			child: GridView.builder(
-				itemCount: buttons.length,
-				gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-					crossAxisCount: 4),
-				itemBuilder: (BuildContext context, int index) {
-					// AC = All Clear Button
-					if (index == 0) {
-					return MyButton(
-						buttontapped: () {
-						setState(() {
-							userInput = '';
-							answer = '0';
-						});
-						},
-						buttonText: buttons[index],
-						color: Color(0xFF7BCFFF),
-						textColor: Color.fromARGB(255, 255, 255, 255),
-					);
-					}
-
-          // C = Delete Button
-					else if (index == 1) {
-					return MyButton(
-						buttontapped: () {
-						setState(() {
-							userInput =	userInput.substring(0, userInput.length - 1);
-						});
-						},
-						buttonText: buttons[index],
-						color: Color(0xFF7BCFFF),
-						textColor: Color.fromARGB(255, 255, 255, 255),
-					);
-					}
-
-					// % Button
-					else if (index == 2) {
-					return MyButton(
-						buttontapped: () {
-						setState(() {
-
-							userInput += buttons[index];
-
-						});
-						},
-						buttonText: buttons[index],
-						color: Color(0xFF7BCFFF),
-						textColor: Color.fromARGB(255, 255, 255, 255),
-					);
-					}
-
-          /*else if (index == 16) {
+                    /*else if (index == 16) {
             return MyButton(
               buttontapped: () {},
               //child: Icon(Icons.mic, color: Colors.black),
             );
 					}*/
 
+                    // Equal_to Button
+                    else if (index == 18) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput += '.';
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.white,
+                        textColor: Color(0xFF101213),
+                      );
+                    } else if (index == 19) {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Color(0xFFF8A3EB),
+                        textColor: Colors.white,
+                      );
+                    }
 
-					// Equal_to Button
-					else if (index == 18) {
-					return MyButton(
-						buttontapped: () {
-						setState(() {
-							userInput += '.';
-						});
-						},
-						buttonText: buttons[index],
-						color: Colors.white,
-						textColor: Color(0xFF101213),
-					);
-					}
-
-          else if (index == 19) {
-					return MyButton(
-						buttontapped: () {
-						setState(() {
-							equalPressed();
-						});
-						},
-						buttonText: buttons[index],
-						color: Color(0xFFF8A3EB),
-						textColor: Colors.white,
-					);
-					}
-
-					// other buttons
-					else {
-					return MyButton(
-						buttontapped: () {
-						setState(() {
-							userInput += buttons[index];
-						});
-						},
-						buttonText: buttons[index],
-						color: isOperator(buttons[index])
-							? Color(0xFFF8A3EB)
-							: Colors.white,
-						textColor: isOperator(buttons[index])
-							? Colors.white
-							: Color(0xFF101213),
-					);
-					}
-				}), // GridView.builder
-			),
-		),
-		],
-	),
-	);
-}
-
-bool isOperator(String x) {
-	if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
-	return true;
-	} /*else if( x == '?'){
-    userInput = '4';
-  }*/
-	return false;
-}
-
-
-// function to calculate the input operation
-void equalPressed() {
-  
-  String finaluserinput(String str){
-    str = str.replaceAll('x', '*');
-    str = str.replaceAll('%', '/100*');
-    return str;
+                    // other buttons
+                    else {
+                      return MyButton(
+                        buttontapped: () {
+                          setState(() {
+                            userInput += buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: isOperator(buttons[index])
+                            ? Color(0xFFF8A3EB)
+                            : Colors.white,
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Color(0xFF101213),
+                      );
+                    }
+                  }), // GridView.builder
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
+  bool isOperator(String x) {
+    if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    } /*else if( x == '?'){
+    userInput = '4';
+  }*/
+    return false;
+  }
 
-	Parser p = Parser();
-	Expression exp = p.parse(finaluserinput(userInput));
-  
-	ContextModel cm = ContextModel();
-	double eval = exp.evaluate(EvaluationType.REAL, cm);
-	answer = eval.toStringAsFixed(5);
-  //answer = eval.toString();
-	//answer = j.toString();
-  //toStringAsExponential(3);
+// function to calculate the input operation
+  void equalPressed() {
+    String finaluserinput(String str) {
+      str = str.replaceAll('x', '*');
+      str = str.replaceAll('%', '/100*');
+      return str;
+    }
+
+    Parser p = Parser();
+    Expression exp = p.parse(finaluserinput(userInput));
+
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    answer = eval.toStringAsFixed(5);
+    //answer = eval.toString();
+    //answer = j.toString();
+    //toStringAsExponential(3);
+  }
 }
-}
-
-
--- calculatorapp/lib/main.dart
+++ calculatorapp/lib/main.dart
@@ -1,6 +1,11 @@
 import 'botton.dart';
 import 'home.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_speech/google_speech.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:sound_stream/sound_stream.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:math_expressions/math_expressions.dart';
 
@@ -11,119 +16,180 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-	  return MaterialApp(
-	  debugShowCheckedModeBanner: false,
-	  home: HomePage(),
-	  ); // MaterialApp
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ); // MaterialApp
   }
 }
 
 class HomePage extends StatefulWidget {
   @override
-    _HomePageState createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final RecorderStream _recorder = RecorderStream();
+  var userInput = '';
+  var responseText = '';
+  var answer = '0';
+
+  bool recognizing = false;
+  bool recognizeFinished = false;
+  String text = '';
+  StreamSubscription<List<int>>? _audioStreamSubscription;
+  BehaviorSubject<List<int>>? _audioStream;
 
   @override
-  var userInput = '';
-  var answer = '0';
+  void initState() {
+    super.initState();
+
+    _recorder.initialize();
+  }
+
+  void streamingRecognize() async {
+    _audioStream = BehaviorSubject<List<int>>();
+    _audioStreamSubscription = _recorder.audioStream.listen((event) {
+      _audioStream!.add(event);
+    });
+
+    await _recorder.start();
+
+    setState(() {
+      recognizing = true;
+    });
+    final serviceAccount = ServiceAccount.fromString(
+        (await rootBundle.loadString('assets/test_service_account.json')));
+    final speechToText = SpeechToText.viaServiceAccount(serviceAccount);
+    final config = _getConfig();
+
+    final responseStream = speechToText.streamingRecognize(
+        StreamingRecognitionConfig(config: config, interimResults: true),
+        _audioStream!);
+
+    responseStream.listen((data) {
+      final currentText =
+          data.results.map((e) => e.alternatives.first.transcript).join('\n');
+
+      if (data.results.first.isFinal) {
+        responseText += '\n' + currentText;
+        setState(() {
+          text = responseText;
+          recognizeFinished = true;
+        });
+      } else {
+        setState(() {
+          text = responseText + '\n' + currentText;
+          recognizeFinished = true;
+        });
+      }
+    }, onDone: () {
+      setState(() {
+        recognizing = false;
+      });
+    });
+  }
+
+  void stopRecording() async {
+    await _recorder.stop();
+    await _audioStreamSubscription?.cancel();
+    await _audioStream?.close();
+    setState(() {
+      recognizing = false;
+    });
+  }
+
+  RecognitionConfig _getConfig() => RecognitionConfig(
+      encoding: AudioEncoding.LINEAR16,
+      model: RecognitionModel.basic,
+      enableAutomaticPunctuation: true,
+      sampleRateHertz: 16000,
+      languageCode: 'th-TH');
 
   @override
   Widget build(BuildContext context) {
-  	return Scaffold(
-    appBar: AppBar(
-    title: Text(
-      "CalculatorForAll",
-      style: TextStyle(
-        fontSize: 25,
-        color: Color(0xFF57636C)
-        )
-      ),
-    
-    backgroundColor: Color(0xFFF1F4F8),
-    iconTheme: IconThemeData(color: Color(0xFF57636C)),
-    actions: [
-    PopupMenuButton(
-      
-      //icon: Icon(Icons.more_vert,color: Color(0xFF57636C)),
-      itemBuilder: (context){
-        return [
-          PopupMenuItem<int>(
-              value: 0,
-              child: Text("Speech",
-              style: TextStyle(color: Color(0xFF57636C))),
-          ),
-          PopupMenuItem<int>(
-              value: 1,
-              child: Text("Calculator",
-              style: TextStyle(color: Color(0xFF57636C))),
-          ),
-        ];
-      },
-      onSelected:(value){
-        if(value == 0){
-          Navigator.push(context,
-           MaterialPageRoute(builder: (context)=>MyApp()));
-        }else if(value == 1){
-          Navigator.push(context,
-           MaterialPageRoute(builder: (context)=>SET()));
-        }
-      }
-    ),
-    ],
-  ),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("CalculatorForAll",
+              style: TextStyle(fontSize: 25, color: Color(0xFF57636C))),
+          backgroundColor: Color(0xFFF1F4F8),
+          iconTheme: IconThemeData(color: Color(0xFF57636C)),
+          actions: [
+            PopupMenuButton(
 
-  backgroundColor: Color(0xFFF1F4F8),
-  body: Column(
-  children: <Widget>[
-		Expanded(
-			child: Container(
-			child: Column(
-				mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-				children: <Widget>[
-					Container(
-					padding: EdgeInsets.all(20),
-					alignment: Alignment.centerRight,
-					child: Text(
-						userInput,
-						style: TextStyle(fontSize: 18, color: Color(0xFF57636C)),
-					),
-					),
-					Container(
-					padding: EdgeInsets.all(15),
-					alignment: Alignment.centerRight,
-					child: Text(
-						answer,
-						style: TextStyle(
-							fontSize: 30,
-							color: Color(0xFF101213),
-							fontWeight: FontWeight.bold),
-					),
-					)
-				]),
-			),
-		),
-    Expanded(
-      child: Container(
-        child: Align(alignment: Alignment.center,
-        child: ElevatedButton(
-          
-          child: Icon(Icons.mic,size: 200),
-              onPressed: () {
-                //ใส่ฟังก์ชัน Speech to text ตรงนี้
-              },
-              style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(20),
-              ),
-        )
+                //icon: Icon(Icons.more_vert,color: Color(0xFF57636C)),
+                itemBuilder: (context) {
+              return [
+                PopupMenuItem<int>(
+                  value: 0,
+                  child: Text("Speech",
+                      style: TextStyle(color: Color(0xFF57636C))),
+                ),
+                PopupMenuItem<int>(
+                  value: 1,
+                  child: Text("Calculator",
+                      style: TextStyle(color: Color(0xFF57636C))),
+                ),
+              ];
+            }, onSelected: (value) {
+              if (value == 0) {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => MyApp()));
+              } else if (value == 1) {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => SET()));
+              }
+            }),
+          ],
         ),
-      )
-    ),
-  ],
-  )
-);
-}
+        backgroundColor: Color(0xFFF1F4F8),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          responseText,
+                          style:
+                              TextStyle(fontSize: 18, color: Color(0xFF57636C)),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          answer,
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Color(0xFF101213),
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ]),
+              ),
+            ),
+            Expanded(
+                child: Container(
+              child: Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  child: Icon(Icons.mic, size: 100),
+                  onPressed:
+                      //ใส่ฟังก์ชัน Speech to text ตรงนี้
+                      recognizing ? stopRecording : streamingRecognize,
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(20),
+                  ),
+                ),
+              ),
+            )),
+          ],
+        ));
+  }
 }
