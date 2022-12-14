59d741e96e19428e6b1587534832598840ef7146
KantapatS <llbumpbumpll@hotmail.com>
2022-12-14 16:31:28 +0700

    add key

--- calculatorapp-re/calculatorapp/lib/services/apiKey.dart
+++ calculatorapp-re/calculatorapp/lib/services/apiKey.dart
@@ -1,5 +1,5 @@
 class APIKeyService {
-  static const _apiKey = "AIzaSyDQpYj7lPnyOY9s7hbuIP-6nH80nkNVCYM";
+  static const _apiKey = "AIzaSyAeHLNdW4ulRVkoPPNk9I6_Gh7GRSGnQZM";
 
   static String fetch() {
     return _apiKey;
